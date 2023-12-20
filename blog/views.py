0b0a46cdb6f8c58860627da12da2702cdc1dd659
oussama3422 <ossamarhayrhay2001@gmail.com>
2023-12-20 21:11:07 +0100

    add pagination

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from .models import Post
-from django.views.generic import DetailView,ListView,CreateView,UpdateView 
+from django.views.generic import DetailView,ListView,CreateView,UpdateView,DeleteView
 
 # dummy_data=[
 #     {
@@ -63,6 +63,7 @@ class PostListView(ListView):
     context_object_name='dummy_data'
     
     ordering=['-date_posted']
+    paginate_by=5
 
    
 class PostDetailView(DetailView):
@@ -76,13 +77,31 @@ class PostCreateView(LoginRequiredMixin,CreateView):
         return super().form_valid(form)
     
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model=Post
     fields=['title','content']
+    
     def form_valid(self,form):
         form.instance.author=self.request.user
         return super().form_valid(form)
     
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 
